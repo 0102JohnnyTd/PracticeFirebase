891aaa28f78e12d92c6d6f72798e7a4eb343070a
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-04 10:12:15 +0900

    保存した値を表示する画面のUIを作成

---
+++ PracticeRealm/ValueListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ValueListTableViewCell.swift
+//  PracticeRealm
+//
+//  Created by Johnny Toda on 2022/07/04.
+//
+
+import UIKit
+
+class ValueListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
