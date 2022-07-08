92519498139e7af79e91944bed3b6b3b19dddb3c
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-08 12:55:03 +0900

    ログインを実行するViewを作成

---
+++ PracticeFirebase/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  PracticeFirebase
+//
+//  Created by Johnny Toda on 2022/07/08.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
