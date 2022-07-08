bbe1a96f14919d12484e45178b77be656a473012
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-08 12:53:11 +0900

    Googleのサインイン機能を実装

--- PracticeFirebase/AppDelegate.swift
+++ PracticeFirebase/AppDelegate.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 import FirebaseCore
-import FirebaseFirestore
-import FirebaseAuth
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -35,6 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        GIDSignIn.sharedInstance.handle(url)
+    }
 }
 
--- PracticeFirebase/RegistValueViewController.swift
+++ PracticeFirebase/RegistValueViewController.swift
@@ -6,8 +6,46 @@
 //
 
 import UIKit
+import GoogleSignIn
+import FirebaseAuth
+import FirebaseCore
 
 class RegistValueViewController: UIViewController {
+    @IBOutlet weak var signInButton: GIDSignInButton!
+
+    @IBAction func signIn(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Google SignIn Success!")
+                }
+            }
+        }
+    }
+
     @IBOutlet private weak var valueTextView: UITextView!
     @IBAction private func registValue(_ sender: Any) {
         completion?(valueTextView.text)
@@ -18,6 +56,5 @@ class RegistValueViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
