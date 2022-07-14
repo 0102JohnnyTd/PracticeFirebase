167abdf7ffafac95ce5687c782cfaa2714f98a73
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-14 16:54:18 +0900

    ログイン後に表示可能となるUserDetailsViewControllerのUITableViewHeaderFooterViewを生成

---
+++ PracticeFirebase/UserDetailsTabViewHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  UserDetailsTabViewHeaderView.swift
+//  PracticeFirebase
+//
+//  Created by Johnny Toda on 2022/07/14.
+//
+
+import UIKit
+
+class UserDetailsTabViewHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet private weak var profileImageView: UIImageView!
+
+    @IBOutlet private weak var nameLabel: UILabel!
+
+    @IBOutlet private weak var mailAddressLabel: UILabel!
+
+
+
+}
