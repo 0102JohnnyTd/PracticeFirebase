0f6ef55fe9cf13c2be7820ba6b4b94fadd65ee30
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-14 17:16:36 +0900

    ログインユーザーの詳細データを表示するUserDetailsViewControllerを作成

---
+++ PracticeFirebase/UserDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UserDetailsViewController.swift
+//  PracticeFirebase
+//
+//  Created by Johnny Toda on 2022/07/14.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
