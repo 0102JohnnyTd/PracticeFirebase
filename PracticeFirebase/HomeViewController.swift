be88f758a0bcc18191eb9dd84aeb8a49596328c1
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-14 10:53:49 +0900

    ・ユーザーがログインしてる時、loginbuttonのタイトルを"プロフィール"と表示させ、ユーザーがログアウトしてる時、"ログイン"と表示させてタップするとauthViewControllerを表示させる機能の実装

--- PracticeFirebase/HomeViewController.swift
+++ PracticeFirebase/HomeViewController.swift
@@ -25,9 +25,65 @@ class HomeViewController: UIViewController {
         navigationController?.pushViewController(registValueVC, animated: true)
     }
 
+    @IBAction private func didTapLoginButton(_ sender: Any) {
+        if Auth.auth().currentUser != nil {
+            showLogoutAlert()
+        } else {
+            showAuthVC()
+        }
+    }
+
     private let userLoginState  = UserLoginState()
+
+    private func logout() {
+        do {
+            try Auth.auth().signOut()
+            let didFinishLogoutAlert = UIAlertController(title: "ログアウト完了", message: "またログインしてね", preferredStyle: .alert)
+            didFinishLogoutAlert.addAction(UIAlertAction(title: "うい。", style: .default))
+            present(didFinishLogoutAlert, animated: true)
+            let title = userLoginState.checkIsLogin(isLogin: userLoginState.getStatus())
+            loginButton.title = title
+        } catch {
+            print(error)
+        }
+    }
+
+    private func showLogoutAlert() {
+            let logoutAlert = UIAlertController(title: "ログアウト", message: "ログアウトしますか？", preferredStyle: .alert)
+            logoutAlert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+        logoutAlert.addAction(UIAlertAction(title: "ログアウト", style: .destructive, handler: { [self] _ in
+                logout()
+            }))
+
+            present(logoutAlert, animated: true, completion: nil)
+        }
+
+    override func viewDidAppear(_ animated: Bool) {
+        if userLoginState.isLogin == false {
+            showAuthVC()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension HomeViewController: FUIAuthDelegate {
+    private func showAuthVC() {
+        if let authUI = FUIAuth.defaultAuthUI() {
+            authUI.providers = [
+                FUIOAuth.appleAuthProvider(withAuthUI: authUI),
+                FUIGoogleAuth(authUI: authUI)
+            ]
+            authUI.delegate = self
+            let authVC = authUI.authViewController()
+            present(authVC, animated: true)
+        }
+    }
+
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        let title = userLoginState.checkIsLogin(isLogin: userLoginState.getStatus())
+        loginButton.title = title
     }
 }
