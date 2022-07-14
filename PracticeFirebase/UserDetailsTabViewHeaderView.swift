3db561015d0a97b80c56e331d1d48d2dff39339e
0102johnny <rt2824.recruit.1022@gmail.com>
2022-07-14 17:13:09 +0900

    UserDetailsViewControllerのUserDetailsTableViewCellを作成

--- PracticeFirebase/UserDetailsTabViewHeaderView.swift
+++ PracticeFirebase/UserDetailsTabViewHeaderView.swift
@@ -8,13 +8,9 @@
 import UIKit
 
 class UserDetailsTabViewHeaderView: UITableViewHeaderFooterView {
-
     @IBOutlet private weak var profileImageView: UIImageView!
 
     @IBOutlet private weak var nameLabel: UILabel!
 
     @IBOutlet private weak var mailAddressLabel: UILabel!
-
-
-
 }
---
+++ PracticeFirebase/UserDetailsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  UserDetailsTableViewCell.swift
+//  PracticeFirebase
+//
+//  Created by Johnny Toda on 2022/07/14.
+//
+
+import UIKit
+
+class UserDetailsTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
